2b565937fa3cc6c02416ac47edc67ece1f7cca8d
Robert Zondervan <Robert@conduction.nl>
2020-06-08 14:39:07 +0200

    Fix proto update

--- api/src/Entity/Product.php
+++ api/src/Entity/Product.php
@@ -24,7 +24,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * )
  *
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
- * @Gedmo\Loggable(logEntryClass="App\Entity\ChangeLog")
+ * @Gedmo\Loggable(logEntryClass="Conduction\CommonGroundBundle\Entity\ChangeLog")
  *
  * @ApiFilter(BooleanFilter::class)
  * @ApiFilter(OrderFilter::class)
