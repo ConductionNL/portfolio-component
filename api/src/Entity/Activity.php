2ce3fc51d3f1d84b7bb9fa36e9d14cff36875c02
Barry Brands <barrybrands02@hotmail.com>
2020-11-20 09:23:35 +0100

    test

--- api/src/Entity/Activity.php
+++ api/src/Entity/Activity.php
@@ -18,6 +18,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Serializer\Annotation\MaxDepth;
 use Symfony\Component\Validator\Constraints as Assert;
 
+
 /**
  * @ApiResource(
  *     normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
